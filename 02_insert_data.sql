ba752793232869512dfc1ff365603da48186cdc7
JuanpiMarinD <126593527+JuanpiMarinD@users.noreply.github.com>
2025-11-25 15:29:08 -0600

    Insert initial data into productos, departamento, and empleado

---
+++ 02_insert_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO productos(nombre,precio,cantidad) values 
+('teclado',180.00,5),
+('mouse',110.00,6),
+('mousepad',100.00,3),
+('monitor',2000.00,2);
+
+INSERT INTO departamento(nombre) values
+('IT'),('RRHH'),('CONTABILIDAD');
+
+INSERT INTO empleado(nombre,id_dep) values
+('Cristian Godinez',1),
+('Juan Marin',2),
+('Edgar Rodriguez',1),
+('Estefano Rodriguez',3);
